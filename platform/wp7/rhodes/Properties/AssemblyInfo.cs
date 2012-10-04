2be4d0006e7d17efab5c8e21dc9b79baff50d6b8
Francisco Luis Aranda <faranda@nosolosoftware.biz>
2012-10-04 12:50:18 +0200

    Custom background image for WP7

--- platform/wp7/rhodes/Properties/AssemblyInfo.cs
+++ platform/wp7/rhodes/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rhodes")]
+[assembly: AssemblyTitle("conTIgo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rhodes")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCompany("Actualize")]
+[assembly: AssemblyProduct("conTIgo")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
